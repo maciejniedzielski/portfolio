28d648cc17079c48d97eff72e31e0b7e3d5414b5
Introoz <niedzielamac@gmail.com>
2017-09-15 22:38:44 +0200

    Editing Portfolio files

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,13 +1,51 @@
-setTimeout(function(){
+$(document).ready(function(){
 
-var list = document.querySelector("ul"),
-    jobTitle = document.querySelector(".job-title");
+  setTimeout(function(){
+    if($("ul.text-decoration").hasClass("hidden")){
+        $("ul.text-decoration").removeClass("hidden");
+    }
+    if($(".job-title").hasClass("hidden")){
+        $(".job-title").removeClass("hidden");
+    }
+  }, 500);
 
-if(list.classList.contains("hidden")){
-	list.classList.remove("hidden");
-}
-if(jobTitle.classList.contains("hidden")){
-	jobTitle.classList.remove("hidden");
-}
+  var navPosition = $(".nav").offset().top;
+  
+  $(window).bind('scroll', function () {
+    if ($(window).scrollTop() > navPosition) {
+        $('.nav').addClass('nav--fixed');
+    } else {
+        $('.nav').removeClass('nav--fixed');
+    }
+  });
 
-}, 1000);
+  $('a[href*="#"]:not([href="#"])').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top-50
+        }, 500);
+    }
+  });
+
+  $(".nav__button").click(function(){
+    $(".nav__menu").toggleClass("toggled");
+  });
+
+  $(".menu__button").click(function(){
+    $(".nav__menu").toggleClass("toggled");
+  });
+
+  var menuLinks= $(".menu__link");
+
+  for (var i = 0; i < menuLinks.length; i++) {
+      var link = menuLinks[i];
+      link.addEventListener('click',function(){
+                $(".nav__menu").removeClass("toggled");
+      }, false);
+    }
+
+});
